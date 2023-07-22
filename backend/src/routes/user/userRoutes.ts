ddd73042878689cf9fcc386bebe9c0783127dc74
KimbohLovette <kimbohlovette@gmail.com>
2023-07-22 17:26:17 +0100

    Added update and delete user routes

--- backend/src/routes/user/userRoutes.ts
+++ backend/src/routes/user/userRoutes.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import { createUser } from '../../controllers/user/createUser';
 import { validatePayload } from '../../middlewares/validatePayload';
 import { getAllUsers } from '../../controllers/user/getAllUsers';
+import { deleteUser } from '../../controllers/user/deleteUserByID';
+import { updateUser } from '../../controllers/user/updateUserByID';
 
 const router = express.Router();
 
 router.post('/', validatePayload, createUser);
 router.get('/', getAllUsers);
+router.delete('/:id', deleteUser);
+router.patch('/:id', updateUser);
 
 export default router;
