636ffed94c3074bc7b621f03a79ca7a980bf26d7
KimbohLovette <kimbohlovette@gmail.com>
2023-07-22 19:11:30 +0100

    Improved error handling for create user

--- backend/src/controllers/user/createUser.ts
+++ backend/src/controllers/user/createUser.ts
@@ -3,15 +3,21 @@ import User from '../../models/user';
 
 export const createUser = async (req: Request, res: Response) => {
 	const payload = req.body;
-	const createdUser = await User.create(payload);
-
-	return res.status(201).json({
-		id: createdUser._id,
-		name: createdUser.name,
-		email: createdUser.email,
-		phoneNumber: createdUser.phoneNumber,
-		createdAt: createdUser.createdAt,
-		updatedAt: createdUser.updatedAt,
-		profession: createdUser.profession,
-	});
+	try {
+		const createdUser = await User.create(payload);
+		return res.status(201).json({
+			id: createdUser._id,
+			name: createdUser.name,
+			email: createdUser.email,
+			phoneNumber: createdUser.phoneNumber,
+			createdAt: createdUser.createdAt,
+			updatedAt: createdUser.updatedAt,
+			profession: createdUser.profession,
+		});
+	} catch (error) {
+		res.status(500).json({
+			error: 'SERVER_ERROR',
+			message: 'An error occured while creating user',
+		});
+	}
 };
