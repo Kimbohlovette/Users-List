0e7e1f178de357de444f0b48f6980a6c22e774c0
KimbohLovette <kimbohlovette@gmail.com>
2023-07-22 16:57:49 +0100

    Configured routes for create user and get all users

--- backend/src/routes/user/userRoutes.ts
+++ backend/src/routes/user/userRoutes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { createUser } from '../../controllers/user/createUser';
+import { validatePayload } from '../../middlewares/validatePayload';
+import { getAllUsers } from '../../controllers/user/getAllUsers';
 
 const router = express.Router();
 
-router.post('/', createUser);
+router.post('/', validatePayload, createUser);
+router.get('/', getAllUsers);
 
 export default router;
