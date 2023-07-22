b5b7329ff4db8e9423bd037bcbe1a855db66815c
KimbohLovette <kimbohlovette@gmail.com>
2023-07-22 16:56:29 +0100

    Implemented the create user endpoint

--- backend/src/controllers/user/createUser.ts
+++ backend/src/controllers/user/createUser.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import User from '../../models/user';
 
 export const createUser = async (req: Request, res: Response) => {
-	return res.status(201).json({ message: 'Hello world' });
+	const payload = req.body;
+	const createdUser = await User.create(payload);
+
+	return res.status(201).json({
+		id: createdUser._id,
+		name: createdUser.name,
+		email: createdUser.email,
+		phoneNumber: createdUser.phoneNumber,
+		createdAt: createdUser.createdAt,
+		updatedAt: createdUser.updatedAt,
+		profession: createdUser.profession,
+	});
 };
